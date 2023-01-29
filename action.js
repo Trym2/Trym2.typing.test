6e891cb491dce69ad6ad60bff7d785f25d8dbc7a
Trym <trym-foss@hotmail.com>
2023-01-29 23:10:55 +0100

    updates

--- action.js
+++ action.js
@@ -1,7 +1,8 @@
 
 const typeText = document.getElementById("typetext")
 const input = document.getElementById("input")
-const scoreText = document.getElementById("score")
+const timer = document.getElementById("timer")
+const wpm = document.getElementById("wpm")
 
 
 let textString = "lorem ipsum dolor sit amet"
@@ -9,17 +10,38 @@ textString = textString.split(" ")
 
 let i = 0;
 let score = 0;
+let time = 30;
+let totalWPM = 0;
 typeText.innerHTML = textString.toString()
-scoreText.innerHTML+= score
+let timerRoll = false;
+
 
 input.addEventListener("keyup", (e) => {
+
+    if (timerRoll) {
+
+    } else {
+        setInterval(() => {
+            if (time > 0) {
+                time = time - 1
+                timer.innerHTML = time.toString()
+                console.log("HI")
+            } else {
+                clearInterval()
+                let calculate = score * 2
+                wpm.innerHTML = calculate.toString()
+            }
+        }, 1000)
+    }
+
+    timerRoll = true;
+
     console.log(e.target.value)
     console.log(textString[i])
     if (e.target.value.includes(" ")) {
 
         if (e.target.value.trim() === textString[i]) {
             score++
-            scoreText.innerHTML = score
             textString[i] = "<span style='color: green'>" + textString[i] + "</span>"
             
             console.log("CORRECT")
@@ -32,3 +54,5 @@ input.addEventListener("keyup", (e) => {
         e.target.value = ""  
     }
 })
+
+
